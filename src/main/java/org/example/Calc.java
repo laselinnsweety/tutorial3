7a844870d638835229fad683e15b094179862cde
laselinnsweety <366499135@qq.com>
2026-09-23 11:32:31 +0800

    add failing testSubtraction test case

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,14 +1,13 @@
 package org.example;
 
 public class Calc {
-
     public int add(int x, int y)
     {
         return x+y;
     }
-
+    // 故意写错！！用来让测试失败
     public int subtract(int x, int y)
     {
-        return x-y;
+        return x+y;
     }
 }
